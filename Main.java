c1c76c823501bc27236a6627d308ea107ba758ec
dybik08 <dybik08@gmail.com>
2017-11-10 14:37:12 +0100

    Update Main.java
    Poprawiony wygląd kodu

--- Main.java
+++ Main.java
@@ -3,22 +3,8 @@
 public class Main 
 {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
          new Frame();
-
-//			double mouseY = 0;
-//			double mouseX = 0;
-//			while(mouseX !=0 || mouseY !=0){			//
-//		 	    mouseX = MouseInfo.getPointerInfo().getLocation().getX();
-//		 	mouseY = MouseInfo.getPointerInfo().getLocation().getY();
-//		 	    String mouseXX = String.valueOf(mouseX);
-//		 	    String mouseYY = String.valueOf(mouseY);
-//		 	    
-//		 	    System.out.println("Pozycja myszy X i Y"+"\n"+"Y" + mouseYY +"X"+ mouseXX);
-		
 	}
-        
-	}
-
-
+}
